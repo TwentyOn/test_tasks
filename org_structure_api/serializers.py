3a90e67c08786a4481598b84448707b6b4a6c3e9
Alexey <aleksey-000@mail.ru>
2026-02-20 23:22:46 +0500

    черновая реализация рекурсивого заполнения дерева объектов department

--- org_structure_api/serializers.py
+++ org_structure_api/serializers.py
@@ -19,3 +19,46 @@ class EmployeeSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'id': {'read_only': True}
         }
+
+
+class DetailDepartmentSerializer(serializers.Serializer):
+    department = serializers.SerializerMethodField()
+    employees = serializers.SerializerMethodField()
+    children = serializers.SerializerMethodField()
+
+    def get_department(self, obj):
+        dep_serializer = DepartmentSerializer(obj)
+        return dep_serializer.data
+
+    def get_employees(self, obj):
+        include_employees = self.context.get('include_employees', 'true')
+        if include_employees.lower() == 'true':
+            data = EmployeeSerializer(obj.employee_set.all().order_by('created_at'), many=True)
+            return data.data
+        else:
+            return None
+
+    def get_children(self, obj):
+        if self.context['cur_depth'] <= int(self.context['depth']):
+            self.context['cur_depth'] += 1
+            children = Department.objects.filter(parent_id=obj.pk)
+            return DetailDepartmentSerializer(instance=children, many=True, context=self.context).data
+        return []
+
+    # def get_children(self, obj):
+    #     depth = int(self.context.get('depth', 1))
+    #     cur_depth = self.context.get('cur_depth')
+    #     print(cur_depth)
+    #
+    #     if cur_depth >= depth:
+    #         return []
+    #     new_context = self.context.copy()
+    #     new_context['cur_depth'] += 1
+    #     children = Department.objects.filter(parent_id=obj.id)
+    #     return DepartmentSerializer(children, many=True, context=new_context).data
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if self.context['include_employees'].lower() == 'false':
+            del data['employees']
+        return data
--- org_structure_api/views.py
+++ org_structure_api/views.py
@@ -1,32 +1,79 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import status, serializers
-from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet, GenericViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.request import Request
 
-from rest_framework.generics import GenericAPIView
-
-from .serializers import DepartmentSerializer, EmployeeSerializer
-from .models import Department
+from .serializers import DepartmentSerializer, EmployeeSerializer, DetailDepartmentSerializer
+from .models import Department, Employee
 
 
 # Create your views here.
-class DepartmentView(ModelViewSet):
+class DepartmentView(GenericViewSet):
     serializer_class = DepartmentSerializer
     queryset = Department.objects.all()
 
-    @action(methods=['get'], detail=True)
-    def get_department(self, request, pk):
-        return Response({'message': 'test'})
+    def create(self, request):
+        """
+        Создание отдела
+        :param request:
+        :return:
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     @action(methods=['post'], detail=True, url_path='employee')
     def create_employee(self, request, pk):
+        """
+        Создание сотрудника
+        :param request:
+        :param pk:
+        :return:
+        """
         department = get_object_or_404(Department, pk=pk)
 
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(department_id=department)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request: Request, pk):
+        department = get_object_or_404(Department, pk=pk)
+
+        depth = request.query_params.get('depth', 1)
+        include_employees = request.query_params.get('include_employees', 'true')
+
+        serializer = DetailDepartmentSerializer(
+            department,
+            context={'include_employees': include_employees, 'depth': depth, 'cur_depth': 1}
+        )
+        data = serializer.data
+        # children = self.get_dep_children(data, include_employees, depth)
+        # data['children'] = children
+
+        return Response(data)
+
+    def get_dep_children(self, initial_data, include_employees, depth, cur_depth=1):
+        print(depth, cur_depth)
+        result = []
+        parent_id = initial_data['department']['id']
+        children = Department.objects.filter(parent_id=parent_id)
+        if children.exists():
+            initial_data['children'] = []
+            for child in children:
+                serializer = DetailDepartmentSerializer(child, context={'include_employees': include_employees})
+                data = serializer.data
+                initial_data['children'].append(data)
+                result.append(data)
+
+        if cur_depth < int(depth):
+            for item in result:
+                self.get_dep_children(item, include_employees, depth, cur_depth + 1)
+        return result
+
 
     def get_serializer_class(self):
         print('action', self.action)
