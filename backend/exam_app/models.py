655c6cd3e9cd72412fff5c1e8b594eb71a254c54
Alexey <aleksey-000@mail.ru>
2026-03-06 19:51:46 +0500

    приложения для реализации задания

---
+++ backend/exam_app/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+from django.contrib.auth.models import User
+from autoslug import AutoSlugField
+
+from common.models import IsDeletedModel, BaseModel
+
+
+class Seller(BaseModel):
+    # Link to the User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="seller")
+
+    # Business Information
+    business_name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from="business_name", always_update=True, unique=True, null=True)
+    inn_identification_number = models.CharField(max_length=50)
+    website_url = models.URLField(null=True)
+    phone_number = models.CharField(max_length=20)
+    business_description = models.TextField()
+
+    # Address Information
+    business_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    # Bank Information
+    bank_name = models.CharField(max_length=255)
+    bank_bic_number = models.CharField(max_length=9)
+    bank_account_number = models.CharField(max_length=50)
+    bank_routing_number = models.CharField(max_length=50)
+
+    # Status fields
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Seller for {self.business_name}"
+
+
+class Category(BaseModel):
+    """
+    Represents a product category.
+
+    Attributes:
+        name (str): The category name, unique for each instance.
+        slug (str): The slug generated from the name, used in URLs.
+        image (ImageField): An image representing the category.
+
+    Methods:
+        __str__():
+            Returns the string representation of the category name.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=True)
+    image = models.ImageField(upload_to='category_images/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+# Create your models here.
+class Product(IsDeletedModel):
+    """
+    Represents a product listed for sale.
+
+    Attributes:
+        seller (ForeignKey): The user who is selling the product.
+        name (str): The name of the product.
+        slug (str): The slug generated from the name, used in URLs.
+        desc (str): A description of the product.
+        price_old (Decimal): The original price of the product.
+        price_current (Decimal): The current price of the product.
+        category (ForeignKey): The category to which the product belongs.
+        in_stock (int): The quantity of the product in stock.
+        image1 (ImageField): The first image of the product.
+        image2 (ImageField): The second image of the product.
+        image3 (ImageField): The third image of the product.
+    """
+
+    seller = models.ForeignKey(Seller, on_delete=models.SET_NULL, related_name="products", null=True)
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from="name", unique=True, db_index=True)
+    desc = models.TextField()
+    price_old = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    price_current = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    in_stock = models.IntegerField(default=5)
+
+    # Only 3 images are allowed
+    image1 = models.ImageField(upload_to='product_images/')
+    image2 = models.ImageField(upload_to='product_images/', blank=True)
+    image3 = models.ImageField(upload_to='product_images/', blank=True)
+
+    def __str__(self):
+        return self.name
