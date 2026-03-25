fff5a6a5005df98c204b59de3f5cdba654c2d44c
Alexey <aleksey-000@mail.ru>
2026-03-25 13:45:38 +0500

    завершение работы над скриптом генерации отчета

--- main.py
+++ main.py
@@ -1,104 +1,161 @@
 import argparse
 import csv
-import inspect
-from pathlib import Path
 import statistics
 from collections import defaultdict
+from abc import ABC, abstractmethod
 
 from tabulate import tabulate
 
-parser = argparse.ArgumentParser(description='Скрипт читает файлы с данными и формирует отчеты')
-parser.add_argument('--files', nargs='+', type=str, required=True, help='путь к файлу/файлам')
-parser.add_argument('--report', '-r', type=str, help='название отчета', choices=['median_coffee'])
-args = parser.parse_args()
+
+class FileReader(ABC):
+    @abstractmethod
+    def can_read(self, filepath: str) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def read(self, filepath: str) -> list[dict]:
+        raise NotImplementedError
 
 
-class Data:
-    def __init__(self):
-        self.data = []
+class CSVReader(FileReader):
+    def can_read(self, filepath: str) -> bool:
+        return filepath.endswith('.csv')
 
-    def read_files(self, files: list | tuple):
-        """
-        Метод заполняет атрибут data данными из нескольких файлов
-        :param files: список путей к файлам
-        :return: None | NotImplemented если разные расширения файлов
-        """
-        for path in files:
-            if path.endswith('.csv'):
-                self.data.extend(self.read_csv(path))
-            else:
-                return NotImplemented
-
-    def read_csv(self, filepath: Path) -> list:
-        """
-        Метод для считывания данных из csv-файла
-        :param filepath: путь к файлу
-        :return: список с
-        """
+    def read(self, filepath: str) -> list[dict]:
         result = []
         with open(filepath, 'r', encoding='utf-8') as csv_file:
             for line in csv.DictReader(csv_file):
                 result.append(line)
         return result
 
-    def prepare_coffee_data(self):
-        result = defaultdict(list)
-        for item in self.data:
-            stud = item['student']
-            result[stud].append(int(item['coffee_spent']))
+
+class ConsoleReport(ABC):
+    """Асбтрактный класс для генерации отчетов"""
+
+    @abstractmethod
+    def generate(self, files: list[str]):
+        """
+        Логика генерации отчета в нужном формате
+        :param files: список путей к файлам
+        :return:
+        """
+        raise NotImplementedError
+
+
+class MedianCoffeeReport(ConsoleReport):
+    def __init__(self, readers: set[FileReader]):
+        self.readers = readers
+
+    def generate(self, files: list[str]) -> str:
+        data = []
+
+        for path in files:
+            for reader in self.readers:
+                if reader.can_read(path):
+                    data.extend(reader.read(path))
+
+        student_coffee_agg = defaultdict(list)
+
+        for item in data:
+            if 'student' not in item or 'coffee_spent' not in item:
+                continue
+
+            student = item['student']
+            num_coffee_spent = float(item['coffee_spent'])
+            student_coffee_agg[student].append(num_coffee_spent)
+
+        str_table = {k: statistics.median(v) for k, v in student_coffee_agg.items()}
+        str_table = dict(sorted(str_table.items(), key=lambda item: item[1], reverse=True))
+
+        headers = ('student', 'median_coffee')
+        str_table = tabulate(str_table.items(), headers=headers, tablefmt='fancy_grid')
+        return str_table
+
+
+class ReadersFactory:
+    _registry = {}
+
+    @classmethod
+    def register(cls, name, reader_cls: FileReader):
+        cls._registry[name] = reader_cls
+
+    @classmethod
+    def create_by_files(cls, files) -> list[FileReader]:
+        result = set()
+        for filename in files:
+            try:
+                extension = filename.split('.')[-1]
+                reader_cls = cls._registry[extension]
+                result.add(reader_cls())
+            except Exception as err:
+                raise ValueError(f'неподдерживаемый формат файла: {filename}')
+
         return result
 
-    """def drop_columns(self, columns: list | tuple | None = None, exclude: list | tuple | None = None):
 
-        if not any((columns, exclude)):
-            raise ValueError(f'Требуется указать 1 из параметров: {inspect.signature(self.drop_columns)}')
+class ReportFactory:
+    _registry = {}
 
-        for item in self.data:
-            if columns:
-                for column in columns:
-                    item.pop(column, None)
-            else:
-                for key in item.copy():
-                    if key not in exclude:
-                        del item[key]
+    @classmethod
+    def register(cls, name, report_cls):
+        cls._registry[name] = report_cls
+
+    @classmethod
+    def create(cls, name, readers):
+        print(name, readers)
+        if name not in cls._registry:
+            raise ValueError(f'Неподдерживаемый отчет: {name}')
+        return cls._registry[name](readers)
+
+
+class Script:
+    """
+    Класс-оркестратор для обработки разных видов отчетов
     """
 
-    """def group_by(self, data: list, by: str, how: str):
-        p = {}
-        result = []
-
-        if how == 'sum':
-            for item in data.copy():
-                by_key = item.pop(by)
-                if by_key not in p:
-                    p[by_key] = item
-                else:
-                    for key in item:
-                        p[by_key][key] = int(p[by_key][key])
-                        p[by_key][key] += int(item[key])
-        else:
-            return NotImplemented
-
-        print(p)
-    """
-
-
-class ReportGenerator:
-    def __init__(self, report_mode: str):
+    def __init__(self, files: list, report_mode: str):
+        self.files = files
         self.report_mode = report_mode
 
-    def generate_report(self, data: Data):
-        if self.report_mode == 'median_coffee':
-            stud_coffee = data.prepare_coffee_data()
-            result = {k: statistics.median(v) for k, v in stud_coffee.items()}
-            print(tabulate(result.items(), headers=['student', self.report_mode], tablefmt="fancy_grid"))
-        else:
-            print(f'значение параметра "report" = {self.report_mode} не поддерживается')
+    def run(self):
+        try:
+            readers = ReadersFactory.create_by_files(self.files)
+            report = ReportFactory.create(self.report_mode, readers)
+
+            print(report.generate(self.files))
+
+        except (ValueError, IOError) as err:
+            print(f'ошибка: {str(err)}')
 
 
-reader = Data()
-reader.read_files(args.files)
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Скрипт читает файлы с данными и формирует отчеты')
+    parser.add_argument('--files', nargs='+', type=str, required=True, help='путь к файлу/файлам')
+    parser.add_argument(
+        '--report',
+        type=str,
+        help='название отчета',
+        required=True,
+    )
+    args = parser.parse_args()
 
-report = ReportGenerator(args.report)
-report.generate_report(reader)
-# report.create_report(reader)
+    return args
+
+
+def main() -> None:
+    readers_factory = ReadersFactory()
+    readers_factory.register('csv', CSVReader)
+
+    report_factory = ReportFactory()
+    report_factory.register('median_coffee', MedianCoffeeReport)
+
+    cmd_args = get_args()
+    report_mode = cmd_args.report
+    files = cmd_args.files
+
+    script = Script(files, report_mode)
+    script.run()
+
+
+if __name__ == '__main__':
+    main()
