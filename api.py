a5ff83a574f6f046bd1b4e2e8a48bd584dd573df
Alexey <aleksey-000@mail.ru>
2026-03-29 20:20:08 +0500

    черновой вариант парсера

---
+++ api.py
@@ -0,0 +1,232 @@
+import asyncio
+import random
+import time
+import traceback
+import urllib.parse
+from time import sleep
+
+import aiohttp
+import requests
+from requests.exceptions import ReadTimeout
+from xlsx_formatter import XLSXFormatter
+
+api_url_form = 'https://www.wildberries.ru/__internal/u-search/exactmatch/ru/common/v18/search?ab_testing=false&appType=1&curr=rub&dest=123585581&hide_vflags=4294967296&lang=ru&page={}&query={}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false'
+api__url_headers = {
+    'authority': 'www.wildberries.ru',
+    'method': 'GET',
+    'path': '/__internal/u-search/exactmatch/ru/common/v18/search?ab_testing=false&appType=1&curr=rub&dest=-1257786&hide_vflags=4294967296&inheritFilters=false&lang=ru&query=%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE+%D0%B8%D0%B7+%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9+%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%B8&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false',
+    'scheme': 'https',
+    'accept': '*/*',
+    'accept-encoding': 'json',
+    'accept-language': 'ru,en;q=0.9',
+    'cookie': '_wbauid=881974751774285031; x_wbaas_token=1.1000.c4aa4909cd4d4840ae01780704311326.MTV8NDUuODguMjAyLjEyMXxNb3ppbGxhLzUuMCAoV2luZG93cyBOVCAxMC4wOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTQ0LjAuMC4wIFlhQnJvd3Nlci8yNi4zLjAuMCBTYWZhcmkvNTM3LjM2fDE3NzU3MjM4MDh8cmV1c2FibGV8MnxleUpvWVhOb0lqb2lJbjA9fDB8M3wxNzc1MTE5MDA4fDE=.MEQCIEMHQJpjKj9MGMVNT+HT2udACR2B+OjlrHYA6cfR6F+aAiBWAiGHaRg0nCZParTXemyj6bl4jW4TNRf0WAjq/wo4eQ==; _cp=1; feedbacks_link_accepted=1',
+    'deviceid': 'site_aa794f41ab3441ed8c0f2471f2d477cd',
+    'priority': 'u=1, i',
+    'referer': 'https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE%20%D0%B8%D0%B7%20%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9%20%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%B8',
+    'sec-ch-ua': '"Not(A:Brand";v="8", "Chromium";v="144", "YaBrowser";v="26.3", "Yowser";v="2.5"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 YaBrowser/26.3.0.0 Safari/537.36,',
+    'x-queryid': 'qid88197475177428503120260328064413',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-spa-version': '14.3.2',
+    'x-userid': '0'
+}
+
+
+class CardParser:
+    def __init__(self):
+        self.api_url_form = 'https://basket-{:02d}.wbbasket.ru/vol{}/part{}/{}/info/ru/card.json'
+        self.headers = {
+            'refer': 'https://www.wildberries.ru/catalog/874597327/detail.aspx?size=1317986564',
+            'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'sec-ch-ua': 'Not(A:Brand";v="8", "Chromium";v="144", "YaBrowser";v="26.3", "Yowser";v="2.5',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': "Windows"
+        }
+
+    async def get_card_json(self, article_id, retry=3):
+        vol = article_id // 100000
+        part = article_id // 1000
+
+        possible_urls = [self.api_url_form.format(buck_id, vol, part, article_id) for buck_id in range(1, 44)]
+        bucket_id, card_json = await self.find_media_url(possible_urls)
+
+        return bucket_id, card_json
+
+    async def find_media_url(self, urls):
+        async with aiohttp.ClientSession() as session:
+            semaphore = asyncio.Semaphore(10)
+
+            async def check_url(url, ind):
+                async with semaphore:
+                    try:
+                        async with session.get(url, headers=self.headers, timeout=5) as resp:
+                            if resp.status == 200:
+                                content = await resp.json()
+                                return ind, content
+                            return None
+                    except Exception as err:
+                        print(traceback.format_exc())
+                        print(url)
+                        return None
+
+            tasks = []
+            for ind, url in enumerate(urls, start=1):
+                task = asyncio.create_task(check_url(url, ind))
+                tasks.append(task)
+
+            for coro in asyncio.as_completed(tasks):
+                result = await coro
+                if result:
+                    # Отмена остальных задач
+                    for task in tasks:
+                        task.cancel()
+                    return result
+            print('не удалось найти карту', urls)
+            return 'None3', dict()
+
+
+async def find_url(urls, headers):
+    async with aiohttp.ClientSession() as session:
+        semaphore = asyncio.Semaphore(10)
+
+        async def check_url(url, ind):
+            async with semaphore:
+                try:
+                    async with session.get(url, headers=headers, timeout=5) as resp:
+                        if resp.status == 200:
+                            content = await resp.json()
+                            return ind, content
+                        return None
+                except Exception as err:
+                    print(traceback.format_exc())
+                    print(url)
+                    return None
+
+        tasks = []
+        for ind, url in enumerate(urls, start=1):
+            task = asyncio.create_task(check_url(url, ind))
+            tasks.append(task)
+
+        for coro in asyncio.as_completed(tasks):
+            result = await coro
+            if result:
+                # Отмена остальных задач
+                for task in tasks:
+                    task.cancel()
+                return result
+        print('не удалось найти карту', urls)
+        return 'None3', dict()
+
+
+async def get_card_json(article_id, retry=3):
+    card_url = 'https://basket-{:02d}.wbbasket.ru/vol{}/part{}/{}/info/ru/card.json'
+
+    card_headers = {
+        'refer': 'https://www.wildberries.ru/catalog/874597327/detail.aspx?size=1317986564',
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'sec-ch-ua': 'Not(A:Brand";v="8", "Chromium";v="144", "YaBrowser";v="26.3", "Yowser";v="2.5',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': "Windows"
+
+    }
+
+    vol = article_id // 100000
+    part = article_id // 1000
+
+    urls = [card_url.format(buck_id, vol, part, article_id) for buck_id in range(1, 44)]
+    bucket_id, card_json = await find_url(urls, card_headers)
+
+    return bucket_id, card_json
+
+
+def get_image_links(bucket_id, article_id, count):
+    url_form = 'https://basket-{:02d}.wbbasket.ru/vol{}/part{}/{}/images/big/{}.webp'
+    vol = article_id // 100000
+    part = article_id // 1000
+    result = []
+    for i in range(1, count + 1):
+        result.append(url_form.format(bucket_id, vol, part, article_id, i))
+
+    return ', '.join(result)
+
+
+async def main(query):
+    page = 1
+    data = []
+    while True:
+        products = requests.get(api_url_form.format(page, urllib.parse.quote(query)), headers=api__url_headers).json()
+        for i, item in enumerate(products['products']):
+            card_parse_start = time.perf_counter()
+            item_data = {}
+            article_id = item['id']
+            print('https://www.wildberries.ru/catalog/{}/detail.aspx'.format(article_id), i + 1)
+            name = item.get('name', 'нет имени')
+
+            price = item.get('sizes')
+            price = price[0] if price else dict()
+            price = price.get('price', dict())
+            price = price.get('product')
+            price = price // 100 if price else 'бесценно'
+
+            sizes = item.get('sizes', dict(name='размеры не указаны'))
+            sizes = ', '.join(size_item['name'] for size_item in sizes)
+
+            seller_name = item.get('supplier', 'продавец не указан')
+            seller_link = item.get('supplierId')
+            seller_link = 'https://www.wildberries.ru/seller/{}'.format(seller_link) if seller_link else 'не указано'
+
+            quantity = item.get('totalQuantity', 'неизвестно')
+            rating = item.get('nmReviewRating', 'неизвестно')
+            rating_count = item.get('feedbacks', 'неизвестно')
+
+            bucket_id, card_json = await get_card_json(article_id)
+            description = card_json.get('description', 'описание не указано')
+
+            specifications = card_json.get('options', [])
+            specifications = [': '.join((op_item['name'], op_item['value'])) for op_item in specifications]
+            specifications = '\n'.join(specifications) if specifications else 'характеристики не указаны'
+
+            image_count = card_json.get('media', dict()).get('photo_count', 0)
+            image_links = get_image_links(bucket_id, article_id, image_count)
+
+            item_data['card_link'] = 'https://www.wildberries.ru/catalog/{}/detail.aspx'.format(article_id)
+            item_data['article'] = article_id
+            item_data['name'] = name
+            item_data['price'] = price
+            item_data['description'] = description
+            item_data['image_links'] = image_links
+            item_data['specifications'] = specifications
+            item_data['seller_name'] = seller_name
+            item_data['seller_link'] = seller_link
+            item_data['sizes'] = sizes
+            item_data['quantity'] = quantity
+            item_data['rating'] = rating
+            item_data['rating_count'] = rating_count
+
+            print('card parse end', time.perf_counter() - card_parse_start)
+            data.append(item_data)
+
+        if len(data) == products['total']:
+            XLSXFormatter().generate_file(data)
+            break
+
+        page += 1
+        print(
+            'кол-во данных спаршено: {}/{} ({:.2f}%)'.format(len(data), products['total'],
+                                                             len(data) / products['total'] * 100))
+        sleep(random.uniform(0.5, 2.0))
+
+
+start = time.perf_counter()
+try:
+    asyncio.run(main('пальто из натуральной шерсти'))
+except Exception as err:
+    print(traceback.format_exc())
+finally:
+    end = time.perf_counter() - start
+    print('парсинг данных завершился за {:.0f}м {:.2f}с'.format(end // 60, end % 60))
---
+++ selenium+BS4.py
@@ -0,0 +1,186 @@
+import time
+from time import sleep
+import urllib.parse
+from collections import defaultdict
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
+
+import xlsxwriter
+
+
+class Parser:
+    def __init__(self):
+        self.driver: WebDriver | None = None
+
+    def get_driver(self, headless):
+        """Настройка Chrome WebDriver."""
+        chrome_options = Options()
+
+        # Основные настройки
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
+
+        # User-Agent (маскируемся под реальный браузер)
+        chrome_options.add_argument(
+            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+
+        if headless:
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--window-size=1920,1080')
+
+        # Создаем драйвер
+        driver = webdriver.Chrome(
+            options=chrome_options
+        )
+
+        # Дополнительная маскировка
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+        self.driver = driver
+
+    def card_parser(self, query, retry=3) -> list[WebElement]:
+        encoded_query = urllib.parse.quote(query)
+
+        self.driver.get(f'https://www.wildberries.ru/catalog/0/search.aspx?search={encoded_query}')
+        wait = WebDriverWait(self.driver, 15)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'product-card__link')))
+        print(self.driver.get_cookies())
+
+        global_start_time = time.perf_counter()
+        sleep(1)
+        heigth = self.driver.execute_script("return document.body.scrollHeight")
+        c_try = 1
+        test_c = 0
+
+        print('Скролл страницы...')
+        while True:
+            test_c += 1
+
+            card_div = self.driver.find_element(By.CLASS_NAME, 'product-card-list')
+            self.driver.execute_script("arguments[0].scrollIntoView({block: 'end'});", card_div)
+            time.sleep(0.2)
+
+            new_h = self.driver.execute_script("return document.body.scrollHeight")
+
+            if test_c == 1:
+                cards = self.driver.find_elements(By.CLASS_NAME, 'product-card__link')
+                break
+
+            if new_h == heigth:
+                if c_try == retry:
+                    cards = self.driver.find_elements(By.CLASS_NAME, 'product-card__link')
+                    break
+                else:
+                    c_try += 1
+                    self.driver.execute_script("window.scrollTo(0, 0);")
+
+            heigth = new_h
+
+        if cards: print(len(cards))
+        duration = time.perf_counter() - global_start_time
+        print('скролл страницы длился {}м {:.2f}c'.format(int(duration // 60), duration % 60))
+        return cards
+
+    def _parse_article(self, html: str) -> str:
+        start = time.perf_counter()
+        soup = BeautifulSoup(html, 'html.parser')
+        article_elem = soup.find(class_='cellValue--hHBJB')
+
+        if article_elem:
+            span_elem = article_elem.find('span')
+            if span_elem:
+                print('нашел артикль за', time.perf_counter() - start)
+                return span_elem.text
+            else:
+                return 'None'
+        else:
+            return 'None'
+
+    def _parse_title(self, html: str) -> str:
+        soup = BeautifulSoup(html, 'html.parser')
+        title_elem = soup.find(class_='productTitle--lfc4o')
+        return title_elem.get_text(strip=True) if title_elem else None
+
+
+    def get_card_html(self, card_url) -> str:
+        start = time.perf_counter()
+        result = {}
+        self.driver.set_window_size(1500, 1080)
+        print(card_url)
+        self.driver.get(card_url)
+        wait = WebDriverWait(self.driver, 15)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'sellerWrap--U2QVn')))
+        sleep(1)
+
+        sizes_elem = self.driver.find_element(By.CLASS_NAME, 'sizesWrap--X6cGL')
+        more_button = sizes_elem.find_elements(By.CLASS_NAME, 'sizesListItemMore--ywCYB')
+        if more_button:
+            more_button.pop().click()
+            sleep(.3)
+        print('загрузил html за ', time.perf_counter() - start)
+        print(self._parse_article(self.driver.page_source))
+
+        return self.driver.page_source
+
+    def run(self):
+        self.get_driver(False)
+        cards = self.card_parser('пальто из натуральной шерсти')
+        urls = [card.get_attribute('href') for card in cards]
+        for url in urls:
+            html = self.get_card_html(url)
+
+
+
+class XLSXFormatter:
+    def __init__(self):
+        self.document = xlsxwriter.Workbook('text.xlsx')
+        self.sheet = self.document.add_worksheet()
+
+    def __write_header(self, headers):
+        for i, header in enumerate(headers):
+            self.sheet.write(0, i, header)
+
+    def __formate_specification(self, value: dict):
+        spec_contet = ''
+        for k, v in value.items():
+            spec_contet += k + '\n'
+            specs = [': '.join(s) for s in v]
+            spec_contet += '\n'.join(specs) + '\n\n'
+
+        return spec_contet
+
+    def generate_file(self, data: list[dict]):
+        headers = [
+            'ссылка на товар', 'артикул', "название", "цена", "описание", "ссылки на изображения",
+            "характеристики", "продавец", "ссылка на продавца", "размеры", "рейтинг", "кол-во отзывов"
+        ]
+        self.__write_header(headers)
+
+        for i, item in enumerate(data, start=1):
+            for j, k in enumerate(item):
+                val = item[k]
+
+                if k == 'specifications':
+                    spec_content = self.__formate_specification(val)
+                    self.sheet.write_string(i, j, spec_content)
+                else:
+                    self.sheet.write_string(i, j, val)
+
+        self.document.close()
+
+
+c = Parser()
+c.get_driver(True)
+c.run()
