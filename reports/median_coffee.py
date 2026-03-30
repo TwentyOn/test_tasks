82cbb85afd91ddd937293b105952780b3642e672
Alexey <aleksey-000@mail.ru>
2026-03-30 17:40:22 +0500

    add: интегрирован класс данных MedianCoffeeReport

--- reports/median_coffee.py
+++ reports/median_coffee.py
@@ -1,20 +1,18 @@
-from reports.base import ConsoleReport
 from collections import defaultdict
 import statistics
+
 from tabulate import tabulate
+
+from reports.base import ConsoleReport
+from models import MedianCoffeeRecord
 
 
 class MedianCoffeeReport(ConsoleReport):
-    def _aggregate(self, data: list[dict]) -> dict[str, list[float]]:
+    def _aggregate(self, data: list[MedianCoffeeRecord]) -> dict[str, list[float]]:
         student_coffee_agg = defaultdict(list)
 
-        for item in data:
-            if 'student' not in item or 'coffee_spent' not in item:
-                continue
-
-            student = item['student']
-            num_coffee_spent = float(item['coffee_spent'])
-            student_coffee_agg[student].append(num_coffee_spent)
+        for record in data:
+            student_coffee_agg[record.student].append(record.coffee_spent)
 
         return student_coffee_agg
 
