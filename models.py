76d9a2b889e1bc4ae44e8bba6ba5676971028f9c
Alexey <aleksey-000@mail.ru>
2026-03-30 19:49:37 +0500

    датакласс для параметров продукта

---
+++ models.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Product:
+    card_link: str
+    article: int
+    name: str
+    price: float | str
+    description: str
+    image_links: str
+    specifications: str
+    seller_name: str
+    seller_link: str
+    sizes: str
+    quantity: int | str
+    rating: float | str
+    rating_count: int | str
