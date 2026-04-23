5030be8280d3b55fcd9dda519e3495fd57f6b320
Alexey <aleksey-000@mail.ru>
2026-04-23 17:11:36 +0500

    add: отчет clickbait

--- reports/reports.py
+++ reports/reports.py
@@ -1,28 +1,16 @@
-from collections import defaultdict
-import statistics
-
-from tabulate import tabulate
-
 from reports.base import ConsoleReport
-from models import MedianCoffeeRecord
+from models import ClickbaitReportRecord, BaseRecord
 
 
 class ClickbaitReport(ConsoleReport):
-    def _aggregate(self, data: list[MedianCoffeeRecord]) -> dict[str, list[float]]:
-        student_coffee_agg = defaultdict(list)
+    def _process(self, read_data: list[BaseRecord]) -> list[ClickbaitReportRecord]:
+        filtered_data = filter(lambda item: item.ctr > 15.0 and item.retention_rate < 40, read_data)
+        filtered_data = [ClickbaitReportRecord.from_base_record(r) for r in filtered_data]
+        sorted_data = sorted(filtered_data, key=lambda item: item.ctr, reverse=True)
+        return sorted_data
 
-        for record in data:
-            student_coffee_agg[record.student].append(record.coffee_spent)
 
-        return student_coffee_agg
-
-    def _calculate(self, aggregate_data: dict) -> dict[str, float]:
-        median_coffe_by_stud = {k: statistics.median(v) for k, v in aggregate_data.items()}
-        median_coffe_by_stud = dict(sorted(median_coffe_by_stud.items(), key=lambda item: item[1], reverse=True))
-        return median_coffe_by_stud
-
-    def render(self, generate_data: dict) -> str:
-        headers = ('student', 'median_coffee')
-        str_table = tabulate(generate_data.items(), headers=headers, tablefmt='fancy_grid')
-
-        return str_table
+# регистрировать новые виды отчетов здесь
+REPORTS: dict[str, type[ConsoleReport]] = {
+    'clickbait': ClickbaitReport
+}
