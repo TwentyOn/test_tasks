8a4277f19e1f40eef73b29c0ad5fa5a8a748c515
Alexey <aleksey-000@mail.ru>
2026-04-23 17:39:55 +0500

    fix: минорные правки

--- main.py
+++ main.py
@@ -1,71 +1,45 @@
 import argparse
 import logging
 
-from reports.median_coffee import MedianCoffeeReport
-from reports.factory import ReportFactory
-
+from reports.reports import REPORTS
 
 logging.basicConfig(level=logging.INFO, format='[{asctime}] #{levelname:4} {name}:{lineno} - {message}', style='{')
 logger = logging.getLogger(__name__)
 
-ALLOWED_REPORTS = ['median_coffee']
+ALLOWED_REPORTS = ['clickbait']
 
 
-class Script:
+def get_args() -> argparse.Namespace:
     """
-    Класс-оркестратор для обработки разных видов отчетов
+    Получение агрументов CLI
+    :return:
     """
+    parser = argparse.ArgumentParser()
 
-    def __init__(self, report_factory: ReportFactory):
-        self.report_factory = report_factory
+    parser.add_argument(
+        f'--files',
+        nargs='+',
+        type=str,
+        required=True,
+        help='путь к файлу/файлам'
+    )
 
-        self.description = 'Скрипт читает файлы с данными и формирует отчеты'
-        self.files_arg = 'files'
-        self.report_arg = 'report'
+    parser.add_argument(
+        f'--report',
+        type=str,
+        help='название отчета',
+        required=True,
+        choices=ALLOWED_REPORTS
+    )
+    args = parser.parse_args()
 
-    def run(self):
-        try:
-            cmd_args = self.get_args()
-            report_mode = cmd_args.report
-            files = cmd_args.files
-
-            report = self.report_factory.create(report_mode)
-            content = report.generate(files)
-
-            print(report.render(content))
-
-        except ValueError as err:
-            logger.error(err)
-
-    def get_args(self) -> argparse.Namespace:
-        parser = argparse.ArgumentParser(description=self.description)
-
-        parser.add_argument(
-            f'--{self.files_arg}',
-            nargs='+',
-            type=str,
-            required=True,
-            help='путь к файлу/файлам'
-        )
-
-        parser.add_argument(
-            f'--{self.report_arg}',
-            type=str,
-            help='название отчета',
-            required=True,
-            choices=ALLOWED_REPORTS
-        )
-        args = parser.parse_args()
-
-        return args
+    return args
 
 
 def main() -> None:
-    report_factory = ReportFactory()
-    report_factory.register('median_coffee', MedianCoffeeReport)
-
-    script = Script(report_factory)
-    script.run()
+    args = get_args()
+    report = REPORTS.get(args.report)()
+    report.generate(args.files)
 
 
 if __name__ == '__main__':
