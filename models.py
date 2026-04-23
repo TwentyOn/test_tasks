72bc73e1f5cc371b6bfc558ce9360d0601ed5823
Alexey <aleksey-000@mail.ru>
2026-04-23 17:09:59 +0500

    add: модели данных (dataclass) для всех данных и отчета clickbait

--- models.py
+++ models.py
@@ -1,15 +1,37 @@
 from dataclasses import dataclass
-import statistics
 
 
 @dataclass
-class MedianCoffeeRecord:
-    student: str
-    coffee_spent: int
+class BaseRecord:
+    title: str
+    ctr: float
+    retention_rate: int
+    views: int
+    likes: int
+    avg_watch_time: float
+
+    def __post_init__(self):
+        self.ctr = float(self.ctr)
+        self.retention_rate = int(self.retention_rate)
+        self.views = int(self.views)
+        self.likes = int(self.likes)
+        self.avg_watch_time = float(self.avg_watch_time)
 
     @classmethod
-    def from_dict(cls, data):
+    def from_dict(cls, data: dict):
+        return cls(**data)
+
+
+@dataclass
+class ClickbaitReportRecord:
+    title: str
+    ctr: float
+    retention_rate: int
+
+    @classmethod
+    def from_base_record(cls, record: BaseRecord):
         return cls(
-            student=data['student'],
-            coffee_spent=data['coffee_spent']
+            title=record.title,
+            ctr=record.ctr,
+            retention_rate=record.retention_rate
         )
